e512b9240691633362a88872e22f1739c346dd66
sabihshahid <sabihshahid1@gmail.com>
2022-08-08 01:38:11 +0500

    second

--- pappu1.js
+++ pappu1.js
@@ -1,5 +1,5 @@
 var city =prompt("where are you from?");
 if (city=="karachi" ||city=="Karachi"){
-    console.log("welcome to city of lights");
+    console.log("welcome to city of light");
 }
 else{console.log("ghair muqamim not allowed")}
